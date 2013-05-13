323dd2e20204e86487fcda99cbd0d2caf905550f
Ian Davis <nospam@iandavis.com>
2013-05-13 12:06:29 +0100

    Reworked to new concurrent architecture

--- fetcher.go
+++ fetcher.go
@@ -7,6 +7,7 @@ import (
 	"github.com/iand/feedparser"
 	"github.com/iand/imgpick"
 	"github.com/iand/salience"
+	// "github.com/mjarco/bloom"
 	"github.com/placetime/datastore"
 	"image/png"
 	"io"
@@ -19,18 +20,20 @@ import (
 )
 
 var (
-	imgDir       = "/var/opt/timescroll/img"
-	feedInterval = 30
-	runOnce      = false
-	feedurl      = ""
+	imgDir        = "/var/opt/timescroll/img"
+	feedInterval  = 10
+	imageInterval = 5
+	runOnce       = false
+	feedurl       = ""
+	workers       = 5
 )
 
 func main() {
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
 	flag.StringVar(&imgDir, "images", "/var/opt/timescroll/img", "filesystem directory to store fetched images")
-	flag.IntVar(&feedInterval, "feedinterval", 30, "interval for checking feeds (minutes)")
+	flag.IntVar(&feedInterval, "feedinterval", 10, "interval for checking feeds (minutes)")
+	flag.IntVar(&imageInterval, "imageInterval", 15, "interval for checking feature images (seconds)")
+	flag.IntVar(&workers, "workers", 5, "number of concurrent workers")
 	flag.BoolVar(&runOnce, "runonce", false, "run the fetcher once and then exit")
 	flag.StringVar(&feedurl, "debugfeed", "", "run the fetcher on the given feed url and debug results")
 	flag.Parse()
@@ -41,21 +44,30 @@ func main() {
 	}
 
 	checkEnvironment()
-	log.Printf("Image directory: %s", imgDir)
+	log.Printf("Images will be written to: %s", imgDir)
 
-	pollFeeds()
-	pollImages()
+	const bufferLength = 0
+
+	quit := make(chan bool)
+
+	jobs := make(chan Job, bufferLength)
 
 	if runOnce {
-		return
+		go worker(1, jobs, quit)
+		pumpOnce(jobs, quit)
+	} else {
+		// Start workers
+		log.Printf("Using %d processor cores", runtime.NumCPU())
+		runtime.GOMAXPROCS(runtime.NumCPU())
+
+		log.Printf("Starting %d workers", workers)
+		for w := 0; w < workers; w++ {
+			go worker(w, jobs, quit)
+		}
+		pumpContinuous(jobs, quit)
 	}
 
-	ticker := time.Tick(30 * time.Minute)
-	for _ = range ticker {
-		pollFeeds()
-		pollImages()
-	}
-
+	close(quit)
 }
 
 func checkEnvironment() {
@@ -76,182 +88,6 @@ func checkEnvironment() {
 		os.Exit(1)
 	}
 
-}
-
-func pollFeeds() {
-	log.Print("Refreshing feeds")
-	s := datastore.NewRedisStore()
-	defer s.Close()
-
-	profiles, _ := s.FeedDrivenProfiles()
-
-	jobs := make(chan *datastore.Profile, len(profiles))
-	results := make(chan *ProfileItemData, len(profiles))
-
-	for w := 0; w < 3; w++ {
-		go feedWorker(w, jobs, results)
-	}
-
-	for _, p := range profiles {
-		jobs <- p
-	}
-	close(jobs)
-
-	for i := 0; i < len(profiles); i++ {
-		data := <-results
-		if data.Error != nil {
-			log.Printf("Error processing feed for %s: %v", data.Profile.Pid, data.Error)
-		} else {
-			log.Printf("Found %d items in feed for %s", len(data.Items), data.Profile.Pid)
-		}
-
-		updateProfileItemData(data)
-		runtime.Gosched()
-	}
-
-}
-
-func pollImages() {
-	log.Print("Fetching images")
-	s := datastore.NewRedisStore()
-	defer s.Close()
-
-	items, _ := s.GrabItemsNeedingImages(30)
-	log.Printf("%d images need to be fetched", len(items))
-	if len(items) > 0 {
-		jobs := make(chan *datastore.Item, len(items))
-		results := make(chan *ItemImageData, len(items))
-
-		for w := 0; w < 3; w++ {
-			go imageWorker(w, jobs, results)
-		}
-
-		for _, p := range items {
-			jobs <- p
-		}
-		close(jobs)
-
-		for i := 0; i < len(items); i++ {
-			data := <-results
-			if data.Error != nil {
-				log.Printf("Error processing images for %s: %v", data.Item.Id, data.Error)
-			} else {
-				log.Printf("Found image %s for %s", data.Item.Image, data.Item.Id)
-			}
-
-			s.UpdateItem(data.Item)
-			runtime.Gosched()
-		}
-	}
-}
-
-type ProfileItemData struct {
-	Profile *datastore.Profile
-	Items   []*datastore.Item
-	Error   error
-}
-
-type ItemImageData struct {
-	Item  *datastore.Item
-	Error error
-}
-
-func feedWorker(id int, jobs <-chan *datastore.Profile, results chan<- *ProfileItemData) {
-	for p := range jobs {
-		log.Printf("Feed worker %d processing feed %s", id, p.FeedUrl)
-
-		resp, err := http.Get(p.FeedUrl)
-
-		if err != nil {
-			log.Printf("Feed worker %d got http error  %s", id, err.Error())
-			results <- &ProfileItemData{p, nil, err}
-			continue
-		}
-		defer resp.Body.Close()
-
-		feed, err := feedparser.NewFeed(resp.Body)
-
-		results <- &ProfileItemData{p, itemsFromFeed(p.Pid, feed), err}
-	}
-}
-
-func itemsFromFeed(pid string, feed *feedparser.Feed) []*datastore.Item {
-
-	items := make([]*datastore.Item, 0)
-	if feed != nil {
-		for _, item := range feed.Items {
-			hasher := md5.New()
-			io.WriteString(hasher, item.Id)
-			id := fmt.Sprintf("%x", hasher.Sum(nil))
-			items = append(items, &datastore.Item{Id: id, Pid: pid, Event: item.When.Unix(), Text: item.Title, Link: item.Link, Image: item.Image})
-		}
-	}
-	return items
-}
-
-func imageWorker(id int, jobs <-chan *datastore.Item, results chan<- *ItemImageData) {
-
-	for item := range jobs {
-		log.Printf("Image worker %d processing item %s", id, item.Id)
-		img, err := imgpick.PickImage(item.Link)
-
-		if img == nil || err != nil {
-			results <- &ItemImageData{item, err}
-			continue
-		}
-
-		imgOut := salience.Crop(img, 460, 160)
-
-		filename := fmt.Sprintf("%s.png", item.Id)
-
-		foutName := path.Join(imgDir, filename)
-
-		fout, err := os.OpenFile(foutName, os.O_CREATE|os.O_WRONLY, 0666)
-		if err != nil {
-			results <- &ItemImageData{item, err}
-			continue
-		}
-
-		if err = png.Encode(fout, imgOut); err != nil {
-			results <- &ItemImageData{item, err}
-			continue
-		}
-
-		item.Image = filename
-
-		results <- &ItemImageData{item, err}
-
-	}
-}
-
-func updateProfileItemData(data *ProfileItemData) error {
-	if data.Items != nil {
-		s := datastore.NewRedisStore()
-		defer s.Close()
-
-		p := data.Profile
-
-		followers, err := s.Followers(p.Pid, p.FollowerCount, 0)
-		if err != nil {
-			return err
-		}
-
-		for _, f := range followers {
-			s.Unfollow(f.Pid, p.Pid)
-		}
-
-		//s.DeleteMaybeItems(p.Pid)
-		for _, item := range data.Items {
-			s.AddItem(item.Pid, time.Unix(item.Event, 0), item.Text, item.Link, item.Image, item.Id)
-		}
-
-		for _, f := range followers {
-			s.Follow(f.Pid, p.Pid)
-		}
-
-	}
-
-	return nil
 }
 
 func debugFeed(url string) {
@@ -275,6 +111,186 @@ func debugFeed(url string) {
 		fmt.Printf("  Image: %s\n", item.Image)
 
 		//		s.AddItem(item.Pid, time.Unix(item.Event, 0), item.Text, item.Link, item.Image, item.Id)
+	}
+
+}
+
+type FetchRecord struct {
+	Url         string `json:"url"`
+	Count       int32  `json:"count"`
+	Interval    int64  `json:"interval"`
+	LastFetched int64  `json:"fetched"`
+	LastChanged int64  `json:"changed"`
+}
+
+func pumpContinuous(jobs chan<- Job, quit <-chan bool) {
+
+	feedTicker := time.NewTicker(time.Duration(feedInterval) * time.Minute)
+	imageTicker := time.NewTicker(time.Duration(imageInterval) * time.Second)
+
+	log.Printf("Waiting %d minutes before fetching feeds", feedInterval)
+	log.Printf("Waiting %d seconds before fetching images", imageInterval)
+
+	for {
+
+		select {
+		case <-quit:
+			return
+		case <-feedTicker.C:
+			pumpRssJobs(jobs)
+
+		case <-imageTicker.C:
+			pumpImageJobs(jobs)
+
+		}
+
+	}
+}
+
+// Execute one cycle of fetching feeds and images
+func pumpOnce(jobs chan<- Job, quit <-chan bool) {
+	pumpRssJobs(jobs)
+	pumpImageJobs(jobs)
+}
+
+func pumpRssJobs(jobs chan<- Job) {
+	s := datastore.NewRedisStore()
+	defer s.Close()
+
+	profiles, _ := s.FeedDrivenProfiles()
+	if len(profiles) == 0 {
+		log.Printf("No feeds require fetching")
+		return
+	}
+	for _, p := range profiles {
+		log.Printf("Pumping feed for profile %s", p.Pid)
+		jobs <- RssJob{Url: p.FeedUrl, Pid: p.Pid}
+	}
+
+}
+
+func pumpImageJobs(jobs chan<- Job) {
+	s := datastore.NewRedisStore()
+	defer s.Close()
+
+	for {
+		items, _ := s.GrabItemsNeedingImages(10)
+		if len(items) == 0 {
+			log.Printf("No items require images to be fetched")
+			return
+		}
+		for _, item := range items {
+			jobs <- ImageJob{Url: item.Link, ItemId: item.Id}
+		}
+	}
+}
+
+type Job interface {
+	Do()
+}
+
+func worker(id int, jobs <-chan Job, quit <-chan bool) {
+	for {
+		select {
+
+		case <-quit:
+			return
+
+		case job := <-jobs:
+			log.Printf("Worker %d processing job", id)
+			job.Do()
+		}
+	}
+}
+
+type RssJob struct {
+	Url string
+	Pid string
+}
+
+func (job RssJob) Do() {
+	log.Printf("RSS job fetching feed at %s", job.Url)
+	resp, err := http.Get(job.Url)
+	defer resp.Body.Close()
+
+	if err != nil {
+		log.Printf("RSS job failed to fetch feed: %s", err.Error())
+		return
+	}
+
+	feed, err := feedparser.NewFeed(resp.Body)
+	if err != nil {
+		log.Printf("RSS job failed to parse feed: %s", err.Error())
+		return
+	}
+
+	s := datastore.NewRedisStore()
+	defer s.Close()
+
+	log.Printf("RSS job found %d items in feed", len(feed.Items))
+
+	for _, item := range feed.Items {
+		hasher := md5.New()
+		io.WriteString(hasher, item.Id)
+		id := fmt.Sprintf("%x", hasher.Sum(nil))
+		_, err := s.AddItem(job.Pid, item.When, item.Title, item.Link, item.Image, id)
+		if err != nil {
+			log.Printf("RSS job failed to add item from feed: %s", err.Error())
+		}
+	}
+
+}
+
+type ImageJob struct {
+	Url    string
+	ItemId string
+}
+
+func (job ImageJob) Do() {
+	log.Printf("Looking for a feature image for %s", job.Url)
+
+	img, err := imgpick.PickImage(job.Url)
+
+	if img == nil || err != nil {
+		log.Printf("Image job failed to pick an image: %s", err.Error())
+		return
+	}
+
+	imgOut := salience.Crop(img, 460, 160)
+
+	filename := fmt.Sprintf("%s.png", job.ItemId)
+
+	foutName := path.Join(imgDir, filename)
+
+	fout, err := os.OpenFile(foutName, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Printf("Image job failed to open image file for writing: %s", err.Error())
+		return
+	}
+
+	if err = png.Encode(fout, imgOut); err != nil {
+		log.Printf("Image job failed to encode image as PNG: %s", err.Error())
+		return
+	}
+
+	log.Printf("Image job wrote image to: %s", foutName)
+
+	s := datastore.NewRedisStore()
+	defer s.Close()
+
+	item, err := s.Item(job.ItemId)
+	if err != nil {
+		log.Printf("Image job failed to get item %s from datastore: %s", job.ItemId, err.Error())
+		return
+	}
+
+	item.Image = filename
+	item.Media = "text"
+
+	err = s.UpdateItem(item)
+	if err != nil {
+		log.Printf("Image job failed to update item %s in datastore: %s", job.ItemId, err.Error())
+		return
 	}
 
 }
